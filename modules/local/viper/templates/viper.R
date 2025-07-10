ff4e9a3a3e156fe07ed8c804271b611bd138d211
ddomenico <domenicd@mskcc.org>
2025-07-10 14:58:49 -0400

    🛠️ fix viper scripts for metaviper implementation

--- modules/local/viper/templates/viper.R
+++ modules/local/viper/templates/viper.R
@@ -4,18 +4,32 @@ library(viper)
 
 # Inject variables
 exp_mat_file <- "${expression_matrix}"
-network_file <- "${network}"
+network_path <- "${network}"
 
 # Load regulon and expression matrix
 exp_mat <- read.table(exp_mat_file, header=TRUE)
 exp_mat\$tmp <- exp_mat\$Unstranded
 
 # Custom Regulon Loading
-regul <- get(load(network_file))
-regul <- pruneRegulon(regul, cutoff=100)
+rda2list <- function(file) {
+    e <- new.env()
+    load(file, envir = e)
+    pruneRegulon(e\$regul, cutoff = 100)
+}
+
+# Check if it exists
+if (!file.exists(network_path)) {
+  message("Network path does not exist.")
+} else if (dir.exists(network_path)) {
+    rda_files <- list.files(network_path, pattern = "\\\\.rda\$", full.names = TRUE)
+    regul_obj <- Map(rda2list, rda_files)
+} else {
+    regul_obj <- get(load(network_path))
+    regul_obj <- pruneRegulon(regul_obj, cutoff=100)
+}
 
 # Run VIPER
-vpres <- viper(exp_mat, regul, verbose = TRUE)
+vpres <- viper(exp_mat, regul_obj, verbose = TRUE)
 colnames(vpres)[1] <- "${meta.id}"
 vpres <- vpres[, "${meta.id}", drop=FALSE]
 
